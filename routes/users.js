a756de76ee9937fb1010fa43695fcfb594f9f8a5
Antonio Melendez <mel23001@byui.edu>
2025-01-17 18:23:25 -0600

    w2 progress

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,23 @@
 const express = require('express');
-//this may be routes in my files REMEMBER
+//This may be routes in my files REMEMBER
 const router = express.Router();
 
 const usersController = require('../controllers/users');
 
+// This is part of RESTful API
+// This route gets all items in the database
 router.get('/', usersController.getAll);
 
+// This route gets a single item from the database
 router.get('/:id', usersController.getSingle);
+
+// This route posts or creates a new item in the database
+router.post('/', usersController.createUser);
+
+// This route updates an item in the database (we can use patch or put)
+router.put('/:id', usersController.updateUser);
+
+// This route deletes an item in the database
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
