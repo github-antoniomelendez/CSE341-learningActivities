d278742ca8889258a9a5b30126c0dc4b8b72c457
Antonio Melendez <mel23001@byui.edu>
2025-01-18 21:16:32 -0600

    swagger

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ routes.get('/', lesson1Controller.antonioRoute);
 routes.get('/aida', lesson1Controller.aidaRoute);
 routes.get('/alessandro', lesson1Controller.alessandroRoute);
 
+// I used the word routes instead of router
+routes.use('/', require('./swagger'));
+
 routes.use('/users', require('./users'));
 
 module.exports = routes;
